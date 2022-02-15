81fd5e2abaf6f9a422aa6aafcb1027c280fa54c6
cmaskito <95998283+cmaskito@users.noreply.github.com>
2022-02-15 22:44:35 +1100

    Update Difficulty.js
    partially cleaned up file

--- screens/Difficulty.js
+++ screens/Difficulty.js
@@ -16,90 +16,83 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Difficulty() {
-  //   const [levelCounters, setLevelCounters] = useState([
-  //     ([levelOneCounter, setLevelOneCounter] = useState({
-  //       title: "Level 1",
-  //       counter: 0,
-  //     })),
-  //     ([levelTwoCounter, setLevelTwoCounter] = useState({
-  //       title: "Level 2",
-  //       counter: 0,
-  //     })),
-  //     ([levelThreeCounter, setLevelThreeCounter] = useState({
-  //       title: "Level 3",
-  //       counter: 0,
-  //     })),
-  //     ([levelFourCounter, setLevelFourCounter] = useState({
-  //       title: "Level 4",
-  //       counter: 0,
-  //     })),
-  //     ([levelFiveCounter, setLevelFiveCounter] = useState({
-  //       title: "Level 5",
-  //       counter: 0,
-  //     })),
-  //     ([levelSixCounter, setLevelSixCounter] = useState({
-  //       title: "Level 6",
-  //       counter: 0,
-  //     })),
-  //     ([levelSevenCounter, setLevelSevenCounter] = useState({
-  //       title: "Level 7",
-  //       counter: 0,
-  //     })),
-  //     ([levelEightCounter, setLevelEightCounter] = useState({
-  //       title: "Level 8",
-  //       counter: 0,
-  //     })),
-  //     ([levelHalfCounter, setLevelHalfCounter] = useState({
-  //       title: "Level 0.5",
-  //       counter: 0,
-  //     })),
-  //   ]);
-
-  [levelOneCounter, setLevelOneCounter] = useState({
-    title: "Level 1",
-    counter: 0,
-  });
-  [levelTwoCounter, setLevelTwoCounter] = useState({
-    title: "Level 2",
-    counter: 0,
-  });
-  [levelThreeCounter, setLevelThreeCounter] = useState({
-    title: "Level 3",
-    counter: 0,
-  });
-  [levelFourCounter, setLevelFourCounter] = useState({
-    title: "Level 4",
-    counter: 0,
-  });
-  [levelFiveCounter, setLevelFiveCounter] = useState({
-    title: "Level 5",
-    counter: 0,
-  });
-  [levelSixCounter, setLevelSixCounter] = useState({
-    title: "Level 6",
-    counter: 0,
-  });
-  [levelSevenCounter, setLevelSevenCounter] = useState({
-    title: "Level 7",
-    counter: 0,
-  });
-  [levelEightCounter, setLevelEightCounter] = useState({
-    title: "Level 8",
-    counter: 0,
-  });
-  [levelHalfCounter, setLevelHalfCounter] = useState({
-    title: "Level 0.5",
-    counter: 0,
-  });
-
   const levelCounters = [
-    { counterState: levelOneCounter, setCounterState: setLevelOneCounter },
-    { counterState: levelTwoCounter, setCounterState: setLevelTwoCounter },
+    ([levelOneCounter, setLevelOneCounter] = useState({
+      title: "Level 1",
+      counter: 0,
+    })),
+    ([levelTwoCounter, setLevelTwoCounter] = useState({
+      title: "Level 2",
+      counter: 0,
+    })),
+    ([levelThreeCounter, setLevelThreeCounter] = useState({
+      title: "Level 3",
+      counter: 0,
+    })),
+    ([levelFourCounter, setLevelFourCounter] = useState({
+      title: "Level 4",
+      counter: 0,
+    })),
+    ([levelFiveCounter, setLevelFiveCounter] = useState({
+      title: "Level 5",
+      counter: 0,
+    })),
+    ([levelSixCounter, setLevelSixCounter] = useState({
+      title: "Level 6",
+      counter: 0,
+    })),
+    ([levelSevenCounter, setLevelSevenCounter] = useState({
+      title: "Level 7",
+      counter: 0,
+    })),
+    ([levelEightCounter, setLevelEightCounter] = useState({
+      title: "Level 8",
+      counter: 0,
+    })),
+    ([levelHalfCounter, setLevelHalfCounter] = useState({
+      title: "Level 0.5",
+      counter: 0,
+    })),
   ];
 
-  const [selectedButton, setSelecteButton] = useState(null);
+  // [levelOneCounter, setLevelOneCounter] = useState({
+  //   title: "Level 1",
+  //   counter: 0,
+  // });
+  // [levelTwoCounter, setLevelTwoCounter] = useState({
+  //   title: "Level 2",
+  //   counter: 0,
+  // });
+  // [levelThreeCounter, setLevelThreeCounter] = useState({
+  //   title: "Level 3",
+  //   counter: 0,
+  // });
+  // [levelFourCounter, setLevelFourCounter] = useState({
+  //   title: "Level 4",
+  //   counter: 0,
+  // });
+  // [levelFiveCounter, setLevelFiveCounter] = useState({
+  //   title: "Level 5",
+  //   counter: 0,
+  // });
+  // [levelSixCounter, setLevelSixCounter] = useState({
+  //   title: "Level 6",
+  //   counter: 0,
+  // });
+  // [levelSevenCounter, setLevelSevenCounter] = useState({
+  //   title: "Level 7",
+  //   counter: 0,
+  // });
+  // [levelEightCounter, setLevelEightCounter] = useState({
+  //   title: "Level 8",
+  //   counter: 0,
+  // });
+  // [levelHalfCounter, setLevelHalfCounter] = useState({
+  //   title: "Level 0.5",
+  //   counter: 0,
+  // });
 
-  const levelOneRef = levelOneCounter;
+  const [selectedButton, setSelecteButton] = useState(null);
 
   const onJudgingButtonPress = (counter) => {
     Vibration.vibrate(70);
@@ -119,16 +112,9 @@ export default function Difficulty() {
       {
         text: "Yes",
         onPress: () => {
-          setLevelOneCounter({ title: "level1", counter: 0 });
-          setLevelTwoCounter(0);
-          setLevelThreeCounter(0);
-          setLevelFourCounter(0);
-          setLevelFiveCounter(0);
-          setLevelSixCounter(0);
-          setLevelSevenCounter(0);
-          setLevelEightCounter(0);
-          setLevelHalfCounter(0);
-          setSelecteButton(null);
+          levelCounters.forEach((item) => {
+            item[1](0);
+          });
         },
       },
     ]);
@@ -167,96 +153,17 @@ export default function Difficulty() {
       {/* Level Buttons */}
       <View style={styles.levelButtonsWrapper}>
         {levelCounters.map((counter, index) => {
-          console.log(counter.counterState);
+          console.log(counter);
           return (
             <CustomButton
               key={index}
               style={styles.levelButton}
-              text={`${counter.counterState.title}\n${counter.counterState.counter}`}
+              text={`${counter[0].title}\n${counter[0].counter}`}
               textStyle={styles.buttonText}
               onPressHandler={() => onJudgingButtonPress(counter)}
             />
           );
         })}
-
-        {/* <CustomButton
-          style={{
-            ...styles.levelButton,
-            backgroundColor: selectedButton === 1 ? "red" : colours.button,
-          }}
-          text={`Level 1\n${levelOneCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelOneCounter, setLevelOneCounter, 1)
-          }
-        />
-        <CustomButton
-          style={{
-            ...styles.levelButton,
-            backgroundColor: selectedButton === 0.5 ? "red" : colours.button,
-          }}
-          text={`Level 0.5\n${levelHalfCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelHalfCounter, setLevelHalfCounter, 0.5)
-          }
-        />
-        <CustomButton
-          style={{
-            ...styles.levelButton,
-            backgroundColor: selectedButton === 4 ? "red" : colours.button,
-          }}
-          text={`Level 4\n${levelFourCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelFourCounter, setLevelFourCounter, 4)
-          }
-        />
-        <CustomButton
-          style={{
-            ...styles.levelButton,
-            backgroundColor: selectedButton === 2 ? "red" : colours.button,
-          }}
-          text={`Level 2\n${levelTwoCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelTwoCounter, setLevelTwoCounter, 2)
-          }
-        />
-        <CustomButton
-          style={{
-            ...styles.levelButton,
-            backgroundColor: selectedButton === 7 ? "red" : colours.button,
-          }}
-          text={`Level 7\n${levelSevenCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelSevenCounter, setLevelSevenCounter, 7)
-          }
-        />
-        <CustomButton
-          style={styles.levelButton}
-          text={`Level 5\n${levelFiveCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelFiveCounter, setLevelFiveCounter)
-          }
-        />
-        <CustomButton
-          style={styles.levelButton}
-          text={`Level 3\n${levelThreeCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelThreeCounter, setLevelThreeCounter)
-          }
-        />
-        <CustomButton
-          style={styles.levelButton}
-          text={`Level 8\n${levelEightCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelEightCounter, setLevelEightCounter)
-          }
-        />
-        <CustomButton
-          style={styles.levelButton}
-          text={`Level 6\n${levelSixCounter}`}
-          onPressHandler={() =>
-            onJudgingButtonPress(levelSixCounter, setLevelSixCounter)
-          }
-        /> */}
       </View>
     </SafeAreaView>
   );
