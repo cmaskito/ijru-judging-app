4aac16bd410f14f143676c0b6541e9a7f4e75c98
cmaskito <95998283+cmaskito@users.noreply.github.com>
2022-03-03 10:21:08 +1100

    Started Creating counter buttons for Elements

--- screens/RequiredElements.js
+++ screens/RequiredElements.js
@@ -20,9 +20,132 @@ import UndoButton from "../components/UndoButton";
 import { Col, Row, Grid } from "react-native-easy-grid";
 
 export default function RequiredElements() {
+  const counters = [
+    ([repeatedSkills, setRepeatedSkills] = useState({
+      title: "Repeated Skills",
+      counter: 0,
+    })),
+    ([gymnasticsPower, setGymnasticsPower] = useState({
+      title: "Gymnastics / Power",
+      counter: 0,
+    })),
+    ([mistakes, setMistakes] = useState({
+      title: "Mistakes",
+      counter: 0,
+    })),
+    ([spaceViolations, setSpaceViolations] = useState({
+      title: "Space Violations",
+      counter: 0,
+    })),
+    ([timeViolations, setTimeViolations] = useState({
+      title: "Time Violations",
+      counter: 0,
+    })),
+    ([multiples, setMultiples] = useState({
+      title: "Multiples",
+      counter: 0,
+    })),
+    ([wrapsReleases, setWrapsReleases] = useState({
+      title: "Wraps / Releases",
+      counter: 0,
+    })),
+  ];
+
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const onJudgingButtonPress = (counter) => {
+    console.log(counter);
+    Vibration.vibrate(70);
+    const newCounter = { ...counter[0], counter: counter[0].counter + 1 };
+    counter[1](newCounter);
+    setSelectedButton(counter[0].title);
+  };
+
   return (
     <SafeAreaView style={AndroidSafeArea.AndroidSafeArea}>
-      <Text>Required Elements</Text>
+      {/* Header */}
+      <Header
+        eventName="Event Name"
+        bracket="Bracket"
+        judgingType="Elements"
+        skipperName="Skipper Name"
+      />
+
+      {/* Red Buttons */}
+      <RedButtons />
+
+      {/* Counter Grid */}
+      <Grid style={styles.countersButtonsWrapper}>
+        <Row size={26.5}>
+          {counters.slice(0, 3).map((counter, index) => (
+            <Col>
+              <CustomButton
+                style={{
+                  ...styles.counterButton,
+                  backgroundColor:
+                    selectedButton === counter[0].title
+                      ? `${colours.button}30`
+                      : colours.button,
+                  borderColor:
+                    selectedButton === counter[0].title
+                      ? `${colours.highlight}`
+                      : colours.button,
+                  borderWidth: 7,
+                  alignSelf:
+                    index === 0
+                      ? "flex-start"
+                      : index === 1
+                      ? "center"
+                      : "flex-end",
+                }}
+                text={`${counter[0].title}\n${counter[0].counter}`}
+                textStyle={styles.buttonText}
+                onPressHandler={() => onJudgingButtonPress(counter)}
+              />
+            </Col>
+          ))}
+        </Row>
+        <Row size={20.5}>
+          {
+            <UndoButton
+              counters={counters}
+              selectedButton={selectedButton}
+              setSelectedButton={setSelectedButton}
+            />
+          }
+        </Row>
+      </Grid>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  redButtonsWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 15,
+    marginHorizontal: 10,
+  },
+  redButton: {
+    backgroundColor: colours.lightRed,
+    width: dimensions.width * 0.29,
+    height: 45,
+    alignSelf: "stretch",
+  },
+  buttonText: {
+    fontFamily: "Roboto_500Medium",
+    fontSize: 18,
+  },
+  countersButtonsWrapper: {
+    marginTop: 16,
+    marginHorizontal: 15,
+    marginBottom: 0,
+    paddingBottom: 0,
+  },
+  counterButton: {
+    width: dimensions.width * 0.29,
+    height: dimensions.height * 0.2,
+    marginBottom: 8,
+    borderRadius: 9,
+  },
+});
