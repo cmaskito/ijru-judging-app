2e1996d76b11bb297f54030b0fae94e5edefedf9
cmaskito <95998283+cmaskito@users.noreply.github.com>
2022-06-23 10:52:38 +1000

    Added this file to make code work

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+﻿const { getDefaultConfig } = require("@expo/metro-config");
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
